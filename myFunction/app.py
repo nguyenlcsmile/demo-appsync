d89389c58c2c61b680e34a12050a4d5e5d328d6f
user-at-704207050840 <caolenguyen@deltavn.vn>
2023-01-03 11:01:36 +0700

    dev

--- myFunction/app.py
+++ myFunction/app.py
@@ -31,7 +31,7 @@ def index(event, context):
     data = json.load(dataJson)
     for i in range(len(data)):
         params = {
-            'value': data[i]
+            'value': f'{data[i]}'
         }
         result = client.execute(document, variable_values=params)
         if (i == 10):
